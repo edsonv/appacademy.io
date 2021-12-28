c4498012baef6ec5d36c512485b274461a70b1ee
Edson J Vargas <edson.jhair@yahoo.com>
2021-12-28 18:28:04 -0400

    .

--- 06 - JavaScript/01 - JavaScript Fundamentals/Projects/Edson_Vargas_js_reversi/src/piece.js
+++ 06 - JavaScript/01 - JavaScript Fundamentals/Projects/Edson_Vargas_js_reversi/src/piece.js
@@ -1,19 +1,22 @@
 /**
  * Initializes the Piece with its color.
  */
-function Piece (color) {
+function Piece(color) {
+  this.color = color;
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+  return (this.color === "black") ? "white" : "black";
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+  this.color = this.oppColor();
 };
 
 /**
@@ -21,10 +24,11 @@ Piece.prototype.flip = function () {
  * based on its color.
  */
 Piece.prototype.toString = function () {
+  return (this.color === "black") ? "B" : "W";
 };
 
 // DON'T TOUCH THIS CODE
-if (typeof window === 'undefined'){
-    module.exports = Piece;
+if (typeof window === 'undefined') {
+  module.exports = Piece;
 }
 // DON'T TOUCH THIS CODE
