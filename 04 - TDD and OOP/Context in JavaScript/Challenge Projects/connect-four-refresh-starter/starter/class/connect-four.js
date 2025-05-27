9f6f72041b0155c92500ae5893be480f1ae1208d
Edson J Vargas <edson.jhair@yahoo.com>
2025-05-27 18:04:37 -0500

    HTML & CSS Unit Tests completed

--- 04 - TDD and OOP/Context in JavaScript/Challenge Projects/connect-four-refresh-starter/starter/class/connect-four.js
+++ 04 - TDD and OOP/Context in JavaScript/Challenge Projects/connect-four-refresh-starter/starter/class/connect-four.js
@@ -1,18 +1,18 @@
-const Screen = require("./screen");
-const Cursor = require("./cursor");
+const Screen = require('./screen');
+const Cursor = require('./cursor');
 
 class ConnectFour {
-
   constructor() {
+    this.playerTurn = 'O';
 
-    this.playerTurn = "O";
-
-    this.grid = [[' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' ']]
+    this.grid = [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ];
 
     this.cursor = new Cursor(6, 7);
 
@@ -29,16 +29,37 @@ class ConnectFour {
 
   // Remove this
   static testCommand() {
-    console.log("TEST COMMAND");
+    console.log('TEST COMMAND');
   }
 
   static checkWin(grid) {
+    const size = grid.length;
+    let isFull = true;
+    const allEqual = (cells) =>
+      cells.every((cell) => cell === cells[0] && cell !== ' ');
 
+    for (let i = 0; i < size; i++) {
+      const row = grid[i];
+      const col = grid.map((row) => row[i]);
+
+      if (allEqual(row)) return row[0]; // Horizontal
+      if (allEqual(col)) return col[0]; // vertical
+
+      if (row.includes(' ')) isFull = false;
+    }
+
+    //  diagonals
+    const mainDiagonal = grid.map((row, i) => row[i]);
+    const antiDiagonal = grid.map((row, i) => row[size - 1 - i]);
+
+    if (allEqual(mainDiagonal)) return mainDiagonal[0];
+    if (allEqual(antiDiagonal)) return antiDiagonal[0];
+
+    return isFull ? 'T' : false;
     // Return 'X' if player X wins
     // Return 'O' if player O wins
     // Return 'T' if the game is a tie
     // Return false if the game has not ended
-
   }
 
   static endGame(winner) {
@@ -52,7 +73,6 @@ class ConnectFour {
     Screen.render();
     Screen.quit();
   }
-
 }
 
 module.exports = ConnectFour;
