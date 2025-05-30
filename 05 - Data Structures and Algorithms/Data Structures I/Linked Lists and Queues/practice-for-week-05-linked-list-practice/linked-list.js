56e2f8c5d94193dd2eaa9409f4f970f8e23b45ca
Edson J Vargas <edson.jhair@yahoo.com>
2025-05-30 16:18:30 -0500

    Linked lists and queues practice 1 completed

---
+++ 05 - Data Structures and Algorithms/Data Structures I/Linked Lists and Queues/practice-for-week-05-linked-list-practice/linked-list.js
@@ -0,0 +1,55 @@
+class LinkedListNode {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    // Your code here
+    this.head = null;
+    this.length = 0;
+  }
+
+  addToHead(val) {
+    // Your code here
+    const newNode = new LinkedListNode(val);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+  }
+
+  addToTail(val) {
+    // Your code here
+    const newNode = new LinkedListNode(val);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+    this.length++;
+  }
+
+  // You can use this function to help debug
+  print() {
+    let current = this.head;
+
+    while (current) {
+      process.stdout.write(`${current.value} -> `);
+      current = current.next;
+    }
+
+    console.log('NULL');
+  }
+}
+
+module.exports = LinkedList;
