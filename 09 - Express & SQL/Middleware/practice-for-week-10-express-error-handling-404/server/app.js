49368577b6c11523df04258a00e9b32024735a46
Edson J Vargas <edson.jhair@yahoo.com>
2025-06-15 15:38:34 -0500

    Error handling middleware

---
+++ 09 - Express & SQL/Middleware/practice-for-week-10-express-error-handling-404/server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('GET / This is the root URL');
+});
+
+app.use((req, res, next) => {
+  const error = new Error("Sorry, the requested resource couldn't be found");
+  error.statusCode = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500);
+  const body = { message: err.message, statusCode: res.statusCode };
+  res.json(body);
+});
+
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port', port));
