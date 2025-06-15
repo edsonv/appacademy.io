2c63253f41aa2e8c298ff73db04a67a52727d10c
Edson J Vargas <edson.jhair@yahoo.com>
2025-06-15 18:19:15 -0500

    express router bonus practice completed

--- 09 - Express & SQL/Intermediate Express/Routers & environmental variables/practice-for-week-10-express-router/server/routes/colors.js
+++ 09 - Express & SQL/Intermediate Express/Routers & environmental variables/practice-for-week-10-express-router/server/routes/colors.js
@@ -3,12 +3,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json('get colors');
+  res.json('GET /colors');
 });
 
 router.get('/:name', (req, res) => {
-  const { name } = req.params;
-  res.json(name);
+  res.json('GET /colors/:name');
+});
+
+router.post('/:name/css-styles', (req, res) => {
+  res.json('POST /colors/:name/css-styles');
+});
+
+router.delete('/:name/css-styles', (req, res) => {
+  res.json('DELETE /colors/:name/css-styles/:style');
 });
 
 module.exports = router;
