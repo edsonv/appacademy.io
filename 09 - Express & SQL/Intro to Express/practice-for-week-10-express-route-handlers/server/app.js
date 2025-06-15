26ff2e2f1ad71a45147356b042b5627fc08cd1c9
Edson J Vargas <edson.jhair@yahoo.com>
2025-06-14 22:14:49 -0500

    Express route handlers practice completed

---
+++ 09 - Express & SQL/Intro to Express/practice-for-week-10-express-route-handlers/server/app.js
@@ -0,0 +1,182 @@
+// Phase 2
+const {
+  getAllArtists,
+  getLatestArtist,
+  getArtistByArtistId,
+  addArtist,
+  editArtistByArtistId,
+  deleteArtistByArtistId,
+  getAlbumsForLatestArtist,
+  getAlbumsByArtistId,
+  getAlbumByAlbumId,
+  addAlbumByArtistId,
+  editAlbumByAlbumId,
+  deleteAlbumByAlbumId,
+  getFilteredAlbums,
+  getSongsByArtistId,
+  getSongsByAlbumId,
+  getSongBySongId,
+  addSongByAlbumId,
+  editSongBySongId,
+  deleteSongBySongId,
+} = require('./data');
+
+const express = require('express');
+const app = express();
+
+// Your code here
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Request Body:', req.body);
+  next();
+});
+
+app.get('/artists', (req, res) => {
+  res.send(getAllArtists());
+});
+
+app.post('/artists', (req, res) => {
+  const response = addArtist(req.body);
+
+  res.status(201);
+  res.send(response);
+});
+
+app.get('/artists/latest', (req, res) => {
+  const response = getLatestArtist();
+  res.send(response);
+});
+
+app.get('/artists/latest/albums', (req, res) => {
+  const response = getAlbumsForLatestArtist();
+  res.send(response);
+});
+
+app.get('/artists/:artistId', (req, res) => {
+  const { artistId } = req.params;
+  const response = getArtistByArtistId(artistId);
+  res.send(response);
+});
+
+app.put('/artists/:artistId', (req, res) => {
+  const { artistId } = req.params;
+  const data = req.body;
+  const response = editArtistByArtistId(artistId, data);
+  res.send(response);
+});
+
+app.patch('/artists/:artistId', (req, res) => {
+  const { artistId } = req.params;
+  const data = req.body;
+  const response = editArtistByArtistId(artistId, data);
+  res.send(response);
+});
+
+app.delete('/artists/:artistId', (req, res) => {
+  const { artistId } = req.params;
+  deleteArtistByArtistId(artistId);
+  res.send({ message: 'Successfully deleted' });
+});
+
+app.get('/artists/:artistId/albums', (req, res) => {
+  const { artistId } = req.params;
+  const response = getAlbumsByArtistId(artistId);
+  res.send(response);
+});
+
+app.get('/albums/:albumId', (req, res) => {
+  const { albumId } = req.params;
+  const response = getAlbumByAlbumId(albumId);
+  res.send(response);
+});
+
+app.post('/artists/:artistId/albums', (req, res) => {
+  const { artistId } = req.params;
+  const data = req.body;
+  const response = addAlbumByArtistId(artistId, data);
+  res.status(201);
+  res.send(response);
+});
+
+app.put('/albums/:albumId', (req, res) => {
+  const { albumId } = req.params;
+  const data = req.body;
+  const response = editAlbumByAlbumId(albumId, data);
+  res.send(response);
+});
+
+app.patch('/albums/:albumId', (req, res) => {
+  const { albumId } = req.params;
+  const data = req.body;
+  const response = editAlbumByAlbumId(albumId, data);
+  res.send(response);
+});
+
+app.delete('/albums/:albumId', (req, res) => {
+  const { albumId } = req.params;
+
+  deleteAlbumByAlbumId(albumId);
+
+  res.send({ message: 'Successfully deleted' });
+});
+
+app.get('/albums?', (req, res) => {
+  const { startsWith } = req.query;
+  const response = getFilteredAlbums(startsWith);
+  res.send(response);
+});
+
+app.get('/songs/:songId', (req, res) => {
+  const { songId } = req.params;
+  const response = getSongBySongId(songId);
+  res.send(response);
+});
+
+app.post('/albums/:albumId/songs', (req, res) => {
+  const { albumId } = req.params;
+  const data = req.body;
+  const response = addSongByAlbumId(albumId, data);
+  res.status(201);
+  res.send(response);
+});
+
+app.get('/artists/:artistId/songs', (req, res) => {
+  const { artistId } = req.params;
+  const response = getSongsByArtistId(artistId);
+  res.send(response);
+});
+
+app.get('/albums/:albumId/songs', (req, res) => {
+  const { albumId } = req.params;
+  const response = getSongsByAlbumId(albumId);
+  res.send(response);
+});
+
+app.put('/songs/:songId', (req, res) => {
+  const { songId } = req.params;
+  const data = req.body;
+  const response = editSongBySongId(songId, data);
+  res.send(response);
+});
+
+app.patch('/songs/:songId', (req, res) => {
+  const { songId } = req.params;
+  const data = req.body;
+  const response = editSongBySongId(songId, data);
+  res.send(response);
+});
+
+app.delete('/songs/:songId', (req, res) => {
+  const { songId } = req.params;
+  deleteSongBySongId(songId);
+  res.send({ message: 'Successfully deleted' });
+});
+
+// DO NOT MODIFY
+if (require.main === module) {
+  const port = 8000;
+  app.listen(port, () => console.log('Server is listening on port', port));
+} else {
+  module.exports = app;
+}
