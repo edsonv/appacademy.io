9535ae358569900cc238357b073248259da6ced0
Edson J Vargas <edson.jhair@yahoo.com>
2025-06-15 14:15:21 -0500

    Serving static files in express bonus practice completed

--- 09 - Express & SQL/Middleware/practice-for-week-10-express-static-files/server/app.js
+++ 09 - Express & SQL/Middleware/practice-for-week-10-express-static-files/server/app.js
@@ -5,6 +5,7 @@ const app = express();
 // app.use('/', express.static(path.join(__dirname, 'assets')));
 app.use('/', express.static(path.join(__dirname, 'assets/scripts')));
 app.use('/stylesheets', express.static(path.join(__dirname, 'assets/css')));
+app.use('/stickers', express.static(path.join(__dirname, 'assets/images')));
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
