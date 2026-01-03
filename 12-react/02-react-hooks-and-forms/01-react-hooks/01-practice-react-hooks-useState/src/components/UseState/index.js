a4854a36390a76d73556fbe7af0f028c6d283d9e
Edson J. Vargas <edson.jhair@yahoo.com>
2026-01-02 20:02:11 -0500

    Added react useState hook practice

---
+++ 12-react/02-react-hooks-and-forms/01-react-hooks/01-practice-react-hooks-useState/src/components/UseState/index.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./UseState.css";
+
+const UseState = () => {
+  const [theme, setTheme] = useState("light");
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className={`state ${theme}`}>
+      <h1>UseState Component</h1>
+      <button
+        onClick={() =>
+          setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"))
+        }
+      >
+        Toggle Theme
+      </button>
+      {/* <button onClick={() => setTheme("light")}>Light</button> */}
+      <h2>{count}</h2>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+        Decrement
+      </button>
+    </div>
+  );
+};
+
+export default UseState;
