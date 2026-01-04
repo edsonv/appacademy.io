70afe8d138ea5b6b3b4336f69b27a2700013eb1b
Edson J. Vargas <edson.jhair@yahoo.com>
2026-01-03 19:14:00 -0500

    Chapter 12 almost completed

---
+++ 12-react/02-react-hooks-and-forms/03-react-forms/01-practice-react-forms/src/ContactUs.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+const ContactUs = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const contactUsInformation = {
+      name,
+      email,
+      phone,
+      submittedOn: new Date(),
+    };
+
+    console.log(contactUsInformation);
+
+    setName("");
+    setEmail("");
+    setPhone("");
+  };
+
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone:</label>
+          <input
+            type="text"
+            id="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </div>
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactUs;
