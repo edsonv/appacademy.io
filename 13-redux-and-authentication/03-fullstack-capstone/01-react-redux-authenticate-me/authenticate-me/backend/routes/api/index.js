5be2e4fdf863dcbcf67cea2c274ccba616de087e
Edson J. Vargas <edson.jhair@yahoo.com>
2026-01-10 00:03:15 -0500

    Authenticate me part 1 completed

--- 13-redux-and-authentication/03-fullstack-capstone/01-react-redux-authenticate-me/authenticate-me/backend/routes/api/index.js
+++ 13-redux-and-authentication/03-fullstack-capstone/01-react-redux-authenticate-me/authenticate-me/backend/routes/api/index.js
@@ -1,6 +1,33 @@
 const router = require("express").Router();
+const asyncHandler = require("express-async-handler");
+const { setTokenCookie } = require("../../utils/auth.js");
+const { restoreUser, requireAuth } = require("../../utils/auth.js");
+const { User } = require("../../db/models");
 
 router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
 });
+
+// tests
+// router.get(
+//   "/set-token-cookie",
+//   asyncHandler(async (_req, res) => {
+//     const user = await User.findOne({
+//       where: {
+//         username: "Demo-lition",
+//       },
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+//   })
+// );
+
+// router.get("/restore-user", restoreUser, (req, res) => {
+//   return res.json(req.user);
+// });
+
+// router.get("/require-auth", requireAuth, (req, res) => {
+//   return res.json(req.user);
+// });
+
 module.exports = router;
